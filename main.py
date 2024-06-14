5e50937a176fe2c0f4ff9959a4e1df7ebcbd62c7
LukaszSzarecki <s179980@student.pg.edu.pl>
2024-06-14 13:05:30 +0200

    Process Discovery

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
-# This is a sample Python script.
+import cairosvg
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# CONSTANTS
+TOTAL_EVENTS_NUM = 10585
+TOTAL_CASES_NUM = 666
+
+# FUNCTIONS
+def convert_svg_to_png(svg_file_path, png_file_path, scale=1.0):
+    # Read the SVG content
+    with open(svg_file_path, 'r') as svg_file:
+        svg_content = svg_file.read()
+
+    # Convert SVG to PNG with high quality
+    cairosvg.svg2png(
+        bytestring=svg_content.encode('utf-8'),
+        write_to=png_file_path,
+        scale=scale,
+        background_color='white'
+    )
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def print_log_info(log_df, total_events_num = TOTAL_EVENTS_NUM, total_cases_num = TOTAL_CASES_NUM):
+    num_events = len(log_df)
+    num_cases = len(log_df['case:concept:name'].unique())
+
+    events_percentage = (num_events / total_events_num) * 100
+    cases_percentage = (num_cases / total_cases_num) * 100
+
+    print("Number of events: {} ({:.2f}%)".format(num_events, events_percentage))
+    print("Number of cases: {} ({:.2f}%)".format(num_cases, cases_percentage))
+    print('\n')
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# USAGE
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Example usage
+svg_file_path = 'charts\\horizon_chart_PMTK.svg'  # Replace with your SVG file path
+png_file_path = 'charts\\horizon_chart_PMTK.png'   # Replace with your desired output PNG file path
+
+convert_svg_to_png(svg_file_path, png_file_path, scale=2.0)
+
+
+
